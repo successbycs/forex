b2489c3c117a996657d702ccea3962e12da230ab
Chris Sparshott <chris@successbycs.com>
2026-08-31 21:27:31 +1200

    Fix M11 GDELT interval filenames

--- tests/milestones/test_m11.py
+++ tests/milestones/test_m11.py
@@ -31,6 +31,7 @@ def test_m11_workflow_builds_all_96_prior_day_intervals_and_retains_no_article_f
 
     assert "minuteOfDay < 24 * 60" in code
     assert "minuteOfDay += 15" in code
+    assert " + '00'" not in code
     assert "payload_sha256" in aggregate_code
     assert "article_text" not in aggregate_code
     assert "headline" not in aggregate_code
